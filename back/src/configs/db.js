2e746ed1db6ee3ccbc855699704511e52c2255a6
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-04-03 19:11:04 +0530

    for deploy

--- back/src/configs/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = ()=>{
-    return mongoose.connect("mongodb+srv://smallcase:small@cluster0.tw22m.mongodb.net/smallcase?retryWrites=true&w=majority");
-}
