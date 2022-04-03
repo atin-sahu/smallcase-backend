68874d57472760d34f307f7b5d1bf6291d399541
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-04-03 19:30:48 +0530

    heroku logs --tail

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const {register,login, generateToken} = require("./controllers/auth_controller");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.use(express.json());
+app.use(cors());
+
+// const {register,login, generateToken} = require("./controllers/auth_controller");
 const smallcasecontroller= require("./controllers/smallcasecont")
 const special_controller = require("./controllers/special_controller");
 const collection_Controller = require("./controllers/all_collection_controller");
@@ -12,14 +20,6 @@ const cors = require("cors");
 
 
 
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-});
-
-app.use(express.json());
-app.use(cors());
 
 // const static_path = path.join(__dirname, "../public");
 // const static_re = path.join(__dirname, "../public/index.html")
@@ -48,16 +48,16 @@ app.use("/smallcase",smallcasecontroller)
 app.use("/specials", special_controller)
 app.use("/collections", collection_Controller);
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] }));
+// app.get('/auth/google',
+//   passport.authenticate('google', { scope: ['profile', 'email'] }));
  
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login', session:false } ),
+// app.get('/auth/google/callback', 
+//   passport.authenticate('google', { failureRedirect: '/login', session:false } ),
 
-  function(req, res) {
-    const token = generateToken(req.user)
-    return res.status(200).send({user:req.user, token})
-  }
-)
+//   function(req, res) {
+//     const token = generateToken(req.user)
+//     return res.status(200).send({user:req.user, token})
+//   }
+// )
 
 module.exports = app;
