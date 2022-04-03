a72977a268001f75d85b018a4b2ec4419380d6b3
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-04-03 13:24:49 +0530

    commit again
    commit again

--- back/src/index.js
+++ back/src/index.js
@@ -45,7 +45,7 @@ app.get('/auth/google',
   passport.authenticate('google', { scope: ['profile', 'email'] }));
  
 app.get('/auth/google/callback', 
-  passport.authenticate('google', { successRedirect: "http://localhost:5000", failureRedirect: '/login', session:false } ),
+  passport.authenticate('google', { failureRedirect: '/login', session:false } ),
 
   function(req, res) {
     const token = generateToken(req.user)
